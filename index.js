1b17fbc702d95a22b4db8be2920dcab5709cb33a
vivinkv6 <vivinkv0@gmail.com>
2022-11-04 19:26:42 +0530

    update

---
+++ index.js
@@ -0,0 +1,48 @@
+
+/* 
+
+tag,id,class
+
+document.getElementById,innerHTML
+document.getElementsByClassName()
+document.getElementsByTagName()
+
+querySelectorAll()
+querySelector()
+*/
+
+
+
+var heading=document.getElementById("h1");
+var btn=document.getElementById("btn")
+
+
+btn.addEventListener("click",changeColor);
+
+function changeColor(){
+    heading.style.color="red"
+}
+
+
+heading.addEventListener(())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,3 @@
+<iframe width="864" height="486" src="https://www.youtube.com/embed/Tn6-PIqc4UM" title="React in 100 Seconds" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+
